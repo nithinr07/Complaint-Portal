7221a7bea3227f9beab0b794097d205583585359
Nithin Raj <nithin.raj@iiitb.org>
2018-08-08 00:27:01 +0530

    show page available

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :tags
-  resources :complaints, :except => [:index, :show]
+  resources :complaints, :except => [:index]
   root 'static_pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
